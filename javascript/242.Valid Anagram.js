8275e43c5dcccf0177a1cadc51357ae9c66f0201
Skyler Minsu Bang <32551541+skylermbang@users.noreply.github.com>
2022-08-09 13:43:05 +1000

    feat add new methode using obj in 242

--- javascript/242.Valid Anagram.js
+++ javascript/242.Valid Anagram.js
@@ -9,44 +9,60 @@ let t3 = "ccac"
 
 
 var isAnagram = function (s, t) {
+    if (s.length != t.length) return false
 
-    let sArray = Array.from(s).sort()
-    let tArray = Array.from(t).sort()
-    let result = true
-    // console.log(sArray, tArray)
+    var hash1 = {}, hash2 = {}
 
-
-    //check the lengths of the two array 
-    if (sArray.length !== tArray.length) {
-        return result = false
-    } else {
-        for (let i = 0; i < sArray.length; i++) {
-
-            let tTarget = tArray[i]
-            let sTarget = sArray[i]
-
-            if (!(sArray.includes(tTarget))) {
-                return result = false
-            }
-            // if (tArray.includes(sTarget)) {
-            //     result = true
-            // } else {
-            //     return result = false
-            // }
-
-
-            if (sTarget !== tTarget) {
-                result = false
-                return result
-            }
-        }
+    for (let char of s) {
+        console.log(char)
+        hash1[char] = hash1[char] + 1 || 1// if none 1 if exist adding one ?
     }
-    //check if there is new character 
+
+    for (let char of t) {
+        hash2[char] = hash2[char] + 1 || 1
+        if (hash2[char] > hash1[char] || !hash1[char]) return false
+    }
+
+    return true
+
+}
+// var isAnagram = function (s, t) {
+
+//     let sArray = Array.from(s).sort()
+//     let tArray = Array.from(t).sort()
+//     let result = true
+//     // console.log(sArray, tArray)
+
+
+//     //check the lengths of the two array 
+//     if (sArray.length !== tArray.length) {
+//         return result = false
+//     } else {
+//         for (let i = 0; i < sArray.length; i++) {
+
+//             let tTarget = tArray[i]
+//             let sTarget = sArray[i]
+
+//             if (!(sArray.includes(tTarget))) {
+//                 return result = false
+//             }
+//             // if (tArray.includes(sTarget)) {
+//             //     result = true
+//             // } else {
+//             //     return result = false
+//             // }
+//             if (sTarget !== tTarget) {
+//                 result = false
+//                 return result
+//             }
+//         }
+//     }
+//     //check if there is new character 
 
 
 
-    return result
-};
+//     return result
+// };
 
 
 console.log(isAnagram(s, t))
